6eb1a61bc10004c37e8bb0ab86be2a059d1d69bd
teresayang1470 <teresayang1470@gmail.com>
2021-05-06 01:20:51 -0700

    demo 2d new frames

--- mpm_taichi_engine/our_demos/demo_2d_new.py
+++ mpm_taichi_engine/our_demos/demo_2d_new.py
@@ -1,5 +1,6 @@
 import taichi as ti
 import numpy as np
+import os
 import utils
 import math
 from engine.mpm_solver import MPMSolver
@@ -16,12 +17,13 @@ mpm = MPMSolver(res=(128, 128), gui=gui)
 for i in range(3):
     mpm.add_cube(lower_corner=[0.2 + i * 0.1, 0.3 + i * 0.1],
                  cube_size=[0.1, 0.1],
-                 material=MPMSolver.material_elastic)
+                 material=MPMSolver.material_elastic,
+                 color=0xED553B)
 
 for frame in range(500):
     mpm.step(8e-3)
     if 10 < frame < 100:
-        mpm.add_cube(lower_corner=[0.8, 0.7],
+        mpm.add_cube(lower_corner=[0.6, 0.7],
                      cube_size=[0.2, 0.01],
                      material=MPMSolver.material_water,
                      color=0x068587)
@@ -39,7 +41,7 @@ for frame in range(500):
     gui.circles(particles['position'],
                 radius=1.5,
                 color=particles['color'])
-    ideo_manager.write_frame(gui.get_image())
+    video_manager.write_frame(gui.get_image())
     gui.show(f'{frame:06d}.png' if write_to_disk else None)
 
 video_manager.make_video(mp4 = False, gif = True)
