390a9f78c91e1a8c8ab215637a87d7d677f3a933
teresayang1470 <teresayang1470@gmail.com>
2021-05-06 00:23:07 -0700

    two slopes

--- mpm_taichi_engine/our_demos/slanted_floor_lava.py
+++ mpm_taichi_engine/our_demos/slanted_floor_lava.py
@@ -11,17 +11,25 @@ gui = ti.GUI("Taichi Elements", res=512, background_color=0x0D1117)
 video_manager = ti.VideoManager(output_dir=os.path.basename(__file__)[:-3]+ "_render/", framerate = 30, automatic_build=False)
 
 mpm = MPMSolver(res=(128, 128), unbounded=True, gui=gui)
-mpm.add_surface_collider(point=(0, 0.0),
+mpm.add_surface_collider(point=(-0.5, 0.8),
+                         normal=(0.3, 1),
+                         surface=mpm.surface_slip)
+mpm.add_surface_collider(point=(-0.5, 0.0),
                          normal=(0.3, 1),
                          surface=mpm.surface_slip)
 
 for frame in range(300):
     mpm.step(8e-3)
-    mpm.add_cube(lower_corner=[0.1, 0.4],
+    mpm.add_cube(lower_corner=[-0.5, 0.8],
                      cube_size=[0.01, 0.05],
                      velocity=[1, 0],
                      color=0xffa500,
                      material=MPMSolver.material_lava)
+    mpm.add_cube(lower_corner=[-0.5, 0.0],
+                     cube_size=[0.01, 0.05],
+                     velocity=[1, 0],
+                     color=0x068587,
+                     material=MPMSolver.material_water)
 
     particles = mpm.particle_info()
     pos = particles['position'] * 0.4 + 0.3
